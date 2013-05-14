2a3ec41e7da280185094970f7d59dfcac1e71de6
martent <marten@thavenius.se>
2013-05-14 18:59:15 +0200

    Removed typeahead

--- app/assets/javascripts/malmo.js
+++ app/assets/javascripts/malmo.js
@@ -1,12 +1,8 @@
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require json2
 //= require jquery.cookie
 //= require jquery-ui.custom.min
 //= require bootstrap
-//= require bootstrap-typeahead
 //= require jquery.tokeninput
 //= require boxes
 //= require masthead_content
