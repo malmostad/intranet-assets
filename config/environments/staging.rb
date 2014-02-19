75ab7f4fd2b9b205e19d4a5c8bcf74b819eda78f
martent <marten@thavenius.se>
2014-02-19 21:37:21 +0100

    Dual deploy config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,18 +1,12 @@
 AssetsContainer::Application.configure do
+  config.cache_classes = true
   config.eager_load = true
-
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-
-  # Raise exceptions instead of rendering exception templates
-  config.action_dispatch.show_exceptions = false
 
   config.assets.digest = false
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
 
-  # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
 end
