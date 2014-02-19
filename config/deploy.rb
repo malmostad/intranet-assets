9a4f4ebaa50f14c773276980ac35c5cae7f2e46b
martent <marten@thavenius.se>
2014-02-19 21:09:03 +0100

    Deployment for 'evolve' assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :server_address, config['server_address']
 server server_address, :web
 set :use_sudo, false
 
-set :stages, %w(staging production)
-set :default_stage, "staging"
+set :stages, %w(staging production evolve)
+set :default_stage, "evolve"
 
 set :application, "assets"
 set :asset_env, "RAILS_GROUPS=assets"
---
+++ config/deploy/evolve.rb
@@ -0,0 +1,2 @@
+set :rails_env, "evolve"
+set :deploy_to, "/var/www/external-assets/3.0/evolve"
